4af61c3a5ca4ce45171dbebafc873225901b60fa
Patrick Ndungutse <52402096+pndungutse@users.noreply.github.com>
2022-12-13 00:13:09 +0200

    Delete Final Project/FinalProjectGroup directory

--- Final Project/FinalProjectGroup/app/src/main/java/com/example/finalprojectgroup/model/Route.kt
+++
@@ -1,5 +0,0 @@
-package com.example.finalprojectgroup.model
-import com.google.firebase.firestore.DocumentId
-data class Route(@DocumentId val id: String, val from: String, val dest:String, val current:String, val current_time:String, val departure_time:String) {
-    constructor(): this( "", "","", "", "", ""){}
-}
